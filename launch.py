b693a731c2e7fac0bd6def7d9f560d908c0b389f
Jaco <jacov@bbd.co.za>
2023-09-20 15:37:51 +0200

    Update launch.py
    Revert

--- launch.py
+++ launch.py
@@ -13,9 +13,9 @@ from modules.path import modelfile_path, lorafile_path, vae_approx_path, fooocus
 REINSTALL_ALL = False
 
 def prepare_environment():
-    # torch_index_url = os.environ.get('TORCH_INDEX_URL', "https://download.pytorch.org/whl/cu118")
-    # torch_command = os.environ.get('TORCH_COMMAND',
-    #                                f"pip install torch==2.0.1 torchvision==0.15.2 --extra-index-url {torch_index_url}")
+    torch_index_url = os.environ.get('TORCH_INDEX_URL', "https://download.pytorch.org/whl/cu118")
+    torch_command = os.environ.get('TORCH_COMMAND',
+                                   f"pip install torch==2.0.1 torchvision==0.15.2 --extra-index-url {torch_index_url}")
     requirements_file = os.environ.get('REQS_FILE', "requirements_versions.txt")
 
     xformers_package = os.environ.get('XFORMERS_PACKAGE', 'xformers==0.0.20')
@@ -27,11 +27,11 @@ def prepare_environment():
     print(f"Fooocus version: {fooocus_version.version}")
 
     comfyui_name = 'ComfyUI-from-StabilityAI-Official'
-    #git_clone(comfy_repo, repo_dir(comfyui_name), "Inference Engine", comfy_commit_hash)
+    git_clone(comfy_repo, repo_dir(comfyui_name), "Inference Engine", comfy_commit_hash)
     sys.path.append(os.path.join(script_path, dir_repos, comfyui_name))
 
-    # if REINSTALL_ALL or not is_installed("torch") or not is_installed("torchvision"):
-    #     run(f'"{python}" -m {torch_command}', "Installing torch and torchvision", "Couldn't install torch", live=True)
+    if REINSTALL_ALL or not is_installed("torch") or not is_installed("torchvision"):
+        run(f'"{python}" -m {torch_command}', "Installing torch and torchvision", "Couldn't install torch", live=True)
 
     if REINSTALL_ALL or not is_installed("xformers"):
         if platform.system() == "Windows":
